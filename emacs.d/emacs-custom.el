72ac71b719b6bd94b209e3145d36ef8bfbc48f6f
Walker <walker@Walkers-MacBook-Air.local>
2017-03-08 16:50:26 -0800

    use babel with source blocks to load emacs config

---
+++ emacs.d/emacs-custom.el
@@ -0,0 +1,26 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(size-indication-mode t)
+ '(inhibit-startup-screen t)
+ '(initial-buffer-choice "~/Dropbox/Org/panopticon.org")
+ '(column-number-mode t)
+ '(indicate-empty-lines t)
+ '(js2-basic-offset 2)
+ '(js2-mode-show-parse-errors nil)
+ '(js2-strict-trailing-comma-warning nil)
+ '(js2-strict-var-redeclaration-warning nil)
+ '(powerline-default-separator (quote wave))
+ '(show-paren-mode t)
+ '(tool-bar-mode nil))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(js2-error ((t nil)))
+ '(js2-external-variable ((t nil)))
+ '(js2-warning ((t nil))))
